f9b247760615e60b7903e516b14854bdfbf4e078
ywyi1992 <ywyi1107@gmail.com>
2020-11-06 16:44:47 +0900

    2020/11/06 설정시작

---
+++ Vue/WebContent/ywy/components/MessageListItem2.js
@@ -0,0 +1,18 @@
+export default{
+	name : "MessageListItem",
+	template : "<li>"
+		  .concat("{{ item.text }} - {{ item.createdAt | datetime }}")
+		  .concat("<button @click='deleteClicked'>X</button>")
+	  .concat("</li>"),
+	props : {
+		item : {
+			type : Object, 
+			required : true
+		}
+	}, 
+	methods : {
+		deleteClicked(){
+			this.$emit("delete");
+		}
+	}
+}
